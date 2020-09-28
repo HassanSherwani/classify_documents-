b5d484f9f554e01b0e83823a85946a3db74f5547
HassanSherwani <45789548+HassanSherwani@users.noreply.github.com>
2020-09-28 13:10:50 +0200

    backup_clean
    cleaning text data on run

--- main.py
+++ main.py
@@ -2,16 +2,23 @@
 from flask import Flask, render_template, url_for, request
 import pickle
 import pandas as pd
+import numpy as np
 import nltk
 import re
 from sklearn.feature_extraction.text import TfidfTransformer
 
 # 2- Start script
+def remove_pattern(input_txt,pattern):
+    r = re.findall(pattern,input_txt)
+    for i in r:
+        input_txt = re.sub(i,'',input_txt)
+    return input_txt
+
 app = Flask(__name__)
 # load saved models
-filename = 'savedmodel/linearsvc_model.sav'
+filename = 'models/linearsvc_model.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
-filename2 = 'savedmodel/tfidf.sav'
+filename2 = 'models/tfidf.sav'
 tfidf = pickle.load(open(filename2, 'rb'))
 
 @app.route('/')
@@ -23,7 +30,9 @@ def predict():
     if request.method == 'POST':
         message = request.form['message']
         data = [message]
-        vect = pd.DataFrame(tfidf.transform(data).toarray())
+        data=pd.DataFrame(data, columns=["body_text"])
+        data['tidy_text'] = np.vectorize(remove_pattern)(data['body_text'],"@[\w]*")
+        vect = pd.DataFrame(tfidf.transform(data.tidy_text).toarray())
         predict_class = loaded_model.predict(vect)
         score=loaded_model._predict_proba_lr(vect)
         prob=score.max(axis=1)
